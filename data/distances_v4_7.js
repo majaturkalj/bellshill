26640a4b91e58d7482c18d7c4eb04dd0e961c53f
majaturkalj <majaturkalj@yahoo.com>
2023-04-26 14:12:08 +0100

    Add files via upload

---
+++ data/distances_v4_7.js
@@ -0,0 +1,1 @@
+var json_distances_v4_7 = {"type":"FeatureCollection","name":"distances_v4_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"distance":"71 m"},"geometry":{"type":"MultiLineString","coordinates":[[[-4.016357676279898,55.805238466758929],[-4.015396998737707,55.804911189012572]]]}}]}
---
+++ data/distances_v5_8.js
@@ -0,0 +1,1 @@
+var json_distances_v5_8 = {"type":"FeatureCollection","name":"distances_v5_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"distance":"57 m"},"geometry":{"type":"MultiLineString","coordinates":[[[-4.016367561901079,55.805239407658497],[-4.016811895163611,55.804799050779401]]]}}]}
